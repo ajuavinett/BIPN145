cf79442e64ede76f5ec6ed1d74a68e9663743d43
Ashley L Juavinett <ajuavine@UCSD.EDU>
2019-02-22 13:50:33 -0800

    initial commit

---
+++ plotOddball.m
@@ -0,0 +1,31 @@
+
+trial = {};
+deviant_IDs = [1,4,7,2,33];
+deviant_trials = [];
+standard_trials = [];
+
+min_trial = min(dataend - datastart);
+
+for iTrial = 1:length(dataend)
+    trial{iTrial} = data(datastart(iTrial):dataend(iTrial));
+        if dataend(iTrial)-datastart(iTrial) > 1200
+            deviant_trials(iTrial,:) = trial{iTrial}(1:min_trial);
+        else
+            standard_trials(iTrial,:) = trial{iTrial}(1:min_trial);
+        end
+end
+
+% average trials
+
+deviant_mean = mean(deviant_trials,'omitnan');
+deviant_mean = deviant_mean - deviant_mean(1);
+standard_mean = mean(standard_trials,'omitnan');
+standard_mean = standard_mean - standard_mean(1);
+
+figure;
+plot(standard_mean)
+hold on
+plot(deviant_mean)
+xlabel('time')
+ylabel('voltage')
+
